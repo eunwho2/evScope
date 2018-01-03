7b381076592f8f1c520dd1ba78ac40d198148204
eunwho <eunwho@gmail.com>
2018-01-03 15:08:59 +0900

    20180103

--- public/js/oscope.js
+++ public/js/oscope.js
@@ -688,4 +688,116 @@ $("document").ready(function() {
   }
 	var dummy = {0:0};
 	socket.emit('codeTable',dummy);
+
+	$("#chart_div3").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+		$("#chart_div4").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+		$("#chart_div5").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+		$("#chart_div6").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+		$("#chart_div7").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+		$("#chart_div8").gauge(70, {
+		min: 0,
+		max: 100,
+		unit: "%",
+		color: "#0080ff",
+		colorAlpha: 1,
+		bgcolor: "#242f43",
+		type: "default"
+	});
+
+
+var chart = AmCharts.makeChart("amchart", {
+  "theme": "light",
+  "type": "gauge",
+  "axes": [{
+    "topTextFontSize": 20,
+    "topTextYOffset": 70,
+    "axisColor": "#31d6ea",
+    "axisThickness": 1,
+    "endValue": 100,
+    "gridInside": true,
+    "inside": true,
+    "radius": "50%",
+    "valueInterval": 10,
+    "tickColor": "#67b7dc",
+    "startAngle": -90,
+    "endAngle": 90,
+    "unit": "%",
+    "bandOutlineAlpha": 0,
+    "bands": [{
+      "color": "#0080ff",
+      "endValue": 100,
+      "innerRadius": "105%",
+      "radius": "170%",
+      "gradientRatio": [0.5, 0, -0.5],
+      "startValue": 0
+    }, {
+      "color": "#3cd3a3",
+      "endValue": 0,
+      "innerRadius": "105%",
+      "radius": "170%",
+      "gradientRatio": [0.5, 0, -0.5],
+      "startValue": 0
+    }]
+  }],
+  "arrows": [{
+    "alpha": 1,
+    "innerRadius": "35%",
+    "nailRadius": 0,
+    "radius": "170%"
+  }]
 });
+
+setInterval(randomValue, 2000);
+
+// set random value
+function randomValue() {
+  var value = Math.round(Math.random() * 100);
+  chart.arrows[0].setValue(value);
+  chart.axes[0].setTopText(value + " %");
+  // adjust darker band to new value
+  chart.axes[0].bands[1].setEndValue(value);
+}
+
+
+ });
+
